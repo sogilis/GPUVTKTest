142834022e1d16a2308852ee7aaf444fbba422a1
victor <victor.sabatier@gmail.com>
2014-06-16 17:04:34 +0200

    use vtkvolumeSmartMApper

--- GPUTest.cxx
+++ GPUTest.cxx
@@ -16,116 +16,50 @@
 // This test volume renders a synthetic dataset with unsigned char values,
 // first with the composite method, then with the MIP method.
 
-#include "vtkSphere.h"
-#include "vtkSampleFunction.h"
-
 #include "vtkGPUVolumeRayCastMapper.h"
-#include "vtkTestUtilities.h"
-#include "vtkColorTransferFunction.h"
-#include "vtkPiecewiseFunction.h"
+#include "vtkSmartVolumeMapper.h"
 #include "vtkRenderer.h"
 #include "vtkRenderWindow.h"
 #include "vtkRenderWindowInteractor.h"
-#include "vtkVolumeProperty.h"
 #include "vtkCamera.h"
-#include "vtkRegressionTestImage.h"
-#include "vtkImageShiftScale.h"
-#include "vtkImageData.h"
-#include "vtkPointData.h"
-#include "vtkDataArray.h"
+#include "vtkSphereSource.h"
+#include <vtkDICOMImageReader.h>
+#include <vtkSmartPointer.h>
 
 int main(int argc, char *argv[])
 {
 
-  // Create a spherical implicit function.
-  vtkSphere *shape=vtkSphere::New();
-  shape->SetRadius(0.1);
-  shape->SetCenter(0.0,0.0,0.0);
+  std::string folder = argv[1];
 
-  vtkSampleFunction *source=vtkSampleFunction::New();
-  source->SetImplicitFunction(shape);
-  shape->Delete();
-  source->SetOutputScalarTypeToDouble();
-  source->SetSampleDimensions(127,127,127); // intentional NPOT dimensions.
-  source->SetModelBounds(-1.0,1.0,-1.0,1.0,-1.0,1.0);
-  source->SetCapping(false);
-  source->SetComputeNormals(false);
-  source->SetScalarArrayName("values");
-
-  source->Update();
-
-  vtkDataArray *a=source->GetOutput()->GetPointData()->GetScalars("values");
-  double range[2];
-  a->GetRange(range);
-
-  vtkImageShiftScale *t=vtkImageShiftScale::New();
-  t->SetInputConnection(source->GetOutputPort());
-  source->Delete();
-  t->SetShift(-range[0]);
-  double magnitude=range[1]-range[0];
-  if(magnitude==0.0)
-    {
-    magnitude=1.0;
-    }
-  t->SetScale(255.0/magnitude);
-  t->SetOutputScalarTypeToUnsignedChar();
-
-  t->Update();
-
+  vtkSmartPointer<vtkDICOMImageReader> reader = vtkSmartPointer<vtkDICOMImageReader>::New();
+   reader->SetDirectoryName(folder.c_str());
+   reader->Update();
+  
   vtkRenderWindow *renWin=vtkRenderWindow::New();
   vtkRenderer *ren1=vtkRenderer::New();
   ren1->SetBackground(0.1,0.4,0.2);
 
   renWin->AddRenderer(ren1);
-  ren1->Delete();
   renWin->SetSize(301,300); // intentional odd and NPOT  width/height
 
   vtkRenderWindowInteractor *iren=vtkRenderWindowInteractor::New();
   iren->SetRenderWindow(renWin);
-  renWin->Delete();
 
-  renWin->Render(); // make sure we have an OpenGL context.
-
-  vtkGPUVolumeRayCastMapper *volumeMapper;
-  vtkVolumeProperty *volumeProperty;
+  vtkSmartVolumeMapper *volumeMapper;
   vtkVolume *volume;
-
-  volumeMapper=vtkGPUVolumeRayCastMapper::New();
+  volumeMapper = vtkSmartVolumeMapper::New();
   volumeMapper->SetBlendModeToComposite(); // composite first
-  volumeMapper->SetInputConnection(
-    t->GetOutputPort());
-
-  volumeProperty=vtkVolumeProperty::New();
-  volumeProperty->ShadeOff();
-  volumeProperty->SetInterpolationType(VTK_LINEAR_INTERPOLATION);
-
-  vtkPiecewiseFunction *mipOpacity = vtkPiecewiseFunction::New();
-  mipOpacity->AddPoint(0.0,0.0);
-  mipOpacity->AddPoint(200.0,0.5);
-  mipOpacity->AddPoint(200.1,1.0);
-  mipOpacity->AddPoint(255.0,1.0);
-
-  vtkPiecewiseFunction *compositeOpacity = vtkPiecewiseFunction::New();
-  compositeOpacity->AddPoint(0.0,0.0);
-  compositeOpacity->AddPoint(80.0,1.0);
-  compositeOpacity->AddPoint(80.1,0.0);
-  compositeOpacity->AddPoint(255.0,0.0);
-  volumeProperty->SetScalarOpacity(compositeOpacity); // composite first.
-
-  vtkColorTransferFunction *color=vtkColorTransferFunction::New();
-  color->AddRGBPoint(0.0  ,0.0,0.0,1.0);
-  color->AddRGBPoint(40.0  ,1.0,0.0,0.0);
-  color->AddRGBPoint(255.0,1.0,1.0,1.0);
-  volumeProperty->SetColor(color);
-  color->Delete();
+  volumeMapper->SetInputConnection(reader->GetOutputPort());
 
   volume=vtkVolume::New();
   volume->SetMapper(volumeMapper);
-  volume->SetProperty(volumeProperty);
   ren1->AddViewProp(volume);
 
-  int valid=volumeMapper->IsRenderSupported(renWin,volumeProperty);
-  if (!valid) {
+  renWin->Render();
+
+  int mode=volumeMapper->GetLastUsedRenderMode();
+  std::cout << mode << std::endl;
+  if (mode != vtkSmartVolumeMapper::GPURenderMode) {
     cout << "GPU extensions not supported." << endl;
   } else {
     cout << "GPU extensions is supported." << endl;
